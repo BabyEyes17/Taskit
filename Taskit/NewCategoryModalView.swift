7693d67ef85a83e136ec687d614cf9d39f4e4914
BabyEyes17 <jaydentlewis@gmail.com>
2026-02-04 20:03:58 -0500

    Initialized main view and modal files | Began work on the Tasks view | Created a temporary hardcoded storage for tasks

---
+++ Taskit/NewCategoryModalView.swift
@@ -0,0 +1,18 @@
+//
+//  NewCategoryModalView.swift
+//  Taskit
+//
+//  Created by Jayden Lewis on 2026-02-04.
+//
+
+import SwiftUI
+
+struct NewCategoryModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewCategoryModalView()
+}
---
+++ Taskit/TaskDeletionConfirmationModalView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskDeletionConfirmationModalView.swift
+//  Taskit
+//
+//  Created by Jayden Lewis on 2026-02-04.
+//
+
+import SwiftUI
+
+struct TaskDeletionConfirmationModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskDeletionConfirmationModalView()
+}
