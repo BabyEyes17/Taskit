db4218d39c67d2f736b230adc28799d4c8e60a5d
Jayden Lewis <jaydent.lewis@gmail.com>
2026-02-08 16:26:05 -0500

    Imported SwiftUI

--- Taskit/TaskitApp.swift
+++ Taskit/TaskitApp.swift
@@ -1,9 +1,14 @@
 // Authored by Jayden Lewis on 08/02/2026
 
+import SwiftUI
+
 @main
 struct TaskitApp: App {
+    
     var body: some Scene {
+        
         WindowGroup {
+            
             TasksView()
         }
     }
