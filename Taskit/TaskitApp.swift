63f198ae2593b7e5bf6dd2f7b746619206f19bf7
Jayden Lewis <jaydent.lewis@gmail.com>
2026-02-08 15:36:31 -0500

    Build error fix

--- Taskit/TaskitApp.swift
+++ Taskit/TaskitApp.swift
@@ -1,10 +1,10 @@
-// Authored by 
+// Authored by Jayden Lewis on 08/02/2026
 
 @main
 struct TaskitApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TasksView()
         }
     }
 }
