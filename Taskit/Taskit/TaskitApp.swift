63cf332254aa7dc49633def74716f725abefd94a
Jayden Lewis <jaydent.lewis@gmail.com>
2026-02-18 09:28:05 -0500

    Downgrade 0.1

---
+++ Taskit/Taskit/TaskitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskitApp.swift
+//  Taskit
+//
+//  Created by Tech on 2026-02-17.
+//
+
+import SwiftUI
+
+@main
+struct TaskitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
