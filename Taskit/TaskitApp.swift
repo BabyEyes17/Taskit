43ba820af5a39dcd44c156b9551bdbb4c0fd828f
Jayden Lewis <jaydentlewis@gmail.com>
2026-03-11 18:03:11 -0700

    Initial Commit

---
+++ Taskit/TaskitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskitApp.swift
+//  Taskit
+//
+//  Created by Jayden Lewis on 2026-03-11.
+//
+
+import SwiftUI
+
+@main
+struct TaskitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
