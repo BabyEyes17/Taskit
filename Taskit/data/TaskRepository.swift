5bfac6b90c3c410efcae8fecc4b4d633026f260a
BabyEyes17 <jaydentlewis@gmail.com>
2026-03-14 00:40:30 -0400

    Core Data Implementation: TaskRepository operations defined

---
+++ Taskit/data/TaskRepository.swift
@@ -0,0 +1,55 @@
+// Authored by Jayden Lewis on 04/02/2026
+
+import CoreData
+import SwiftUI
+
+struct TaskStore {
+    
+    static func addTask(
+        
+        title: String,
+        taskDescription: String,
+        category: String,
+        dueDate: Date,
+        notificationsEnabled: Bool,
+        notifyBeforeMinutes: Int16,
+        repeatRule: String,
+        tags: [String],
+        context: NSManagedObjectContext
+    ) {
+        
+        let task = TaskEntity(context: context)
+        
+        task.id = UUID()
+        task.title = title
+        task.taskDescription = taskDescription
+        task.category = category
+        task.dueDate = dueDate
+        task.notificationsEnabled = notificationsEnabled
+        task.notifyBeforeMinutes = notifyBeforeMinutes
+        task.repeatRule = repeatRule
+        task.tags = tags as NSArray
+        task.isCompleted = false
+        task.isFavourite = false
+        
+        save(context: context)
+    }
+    
+    static func deleteTask(_ task: TaskEntity, context: NSManagedObjectContext) {
+        
+        context.delete(task)
+        save(context: context)
+    }
+    
+    static func toggleCompleted( _ task: TaskEntity, context: NSManagedObjectContext) {
+        
+        task.isCompleted.toggle()
+        save(context: context)
+    }
+    
+    static func save(context: NSManagedObjectContext) {
+        
+        guard context.hasChanges else { return }
+        try? context.save
+    }
+}
